c4f75c6cf3c0b5c497098cbeda23b0d91b6f2884
cdtest <corydavis1989@gmail.com>
2022-05-05 11:17:04 -0500

    work with useState and useEffect hooks/dynamically altering the document title

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,25 +1,24 @@
-import React from "react";
-import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
+import React from 'react'
+import { capitalizeFirstLetter  } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg"
 
-function Gallery(props) {
-  const currentCategory = {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  };
+const Gallery = () => {
+
+const currentCategory = {
+  name: "commmercial",
+  desription: "Photos of grocery stores, food trucks, and other commercial projects",
+};
+
+
   return (
     <section>
       <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.name}</p>
-      <div className="flex-row">
-          <img
-            src={photo}
-            alt="Commercial Example"
-            className="img-thumbnail mx-1"
-          />
+      <p>{currentCategory.desription}</p>
+      <div className='flex-row'>
+        <img src={photo} alt="groceries" className='img-thumbnail mx-1' />
       </div>
     </section>
-  );
+  )
 }
-export default Gallery;
+
+export default Gallery
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,7 +10,7 @@ function Nav(props) {
   } = props;
 
   useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
+    document.title = capitalizeFirstLetter(currentCategory.name)
   }, [currentCategory]);
 
   return (
